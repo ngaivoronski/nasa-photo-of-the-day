6b4d5adb7ec085e206341b0375ece5cc47f6e128
ngaivoronski <ngaivoronski@gmail.com>
2019-10-10 10:50:04 -0400

    added dropdown date select

--- src/components/NasaGrid.js
+++ src/components/NasaGrid.js
@@ -1,21 +1,53 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, Component} from "react";
 import axios from "axios";
 import PhotoCard from "./PhotoCard";
+import moment from 'moment';
+import Dropdown from 'react-dropdown'
+import classNames from 'classnames/bind';
+import 'react-dropdown/style.css';
 
 export default function NasaGrid() {
     const [photoData, setPhotoData] = useState([]);
-    const [date, setDate] = useState("");
+
+    var today = moment().format("YYYY-MM-DD");
+    var yesterday = moment().subtract(1, 'days').format("YYYY-MM-DD");
+    var twoDaysAgo = moment().subtract(2, 'days').format("YYYY-MM-DD");
+    var threeDaysAgo = moment().subtract(3, 'days').format("YYYY-MM-DD");
+    var fourDaysAgo = moment().subtract(4, 'days').format("YYYY-MM-DD");
+
+    const options = [today, yesterday, twoDaysAgo, threeDaysAgo, fourDaysAgo];
+    
+    
+
+    function changeDate() {
+        setTimeout(function(){
+            var menu = document.querySelector('.is-selected').innerHTML;
+            console.log(menu);
+            setDate(menu);
+            console.log(date);
+            setCurrentOption(menu);
+            
+            }, 10);
+    }
+
+    const [date, setDate] = useState(today);
+
+    const [currentOption, setCurrentOption] = useState(date);
+
     useEffect(() => {
         axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=t3F5j6ddCIh7btoXDgbTFmLp5COSAOfvqvQ03bUd`)
+        .get(`https://api.nasa.gov/planetary/apod?api_key=t3F5j6ddCIh7btoXDgbTFmLp5COSAOfvqvQ03bUd&date=${date}`)
         .then(response => {
             console.log(response);
             setPhotoData(response.data);
         });
-    }, []);
+    }, [date]);
+    
+    
 
     return (
         <div>
+            <Dropdown id="dropdown" options={options} onChange={changeDate} value={currentOption} placeholder="Select an option" />
             <PhotoCard title={photoData.title} url={photoData.url} date={photoData.date} explanation={photoData.explanation} />
         </div>
     );
