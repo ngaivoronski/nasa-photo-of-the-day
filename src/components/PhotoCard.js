df43a25ccfff0cacf6c8982f223c95e2ff434dbe
ngaivoronski <ngaivoronski@gmail.com>
2019-10-11 02:06:58 -0400

    styling changes

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,13 +1,34 @@
 import React from "react";
+import styled from 'styled-components'
+
+const PhotoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: auto;
+    width: 80%
+`
+
+const Title = styled.h2`
+    @import url('https://fonts.googleapis.com/css?family=Orbitron|Roboto&display=swap');
+
+    font-family: 'Orbitron', sans-serif;
+    color: slategrey;
+`;
+
+const CardP = styled.p `
+    @import url('https://fonts.googleapis.com/css?family=Orbitron|Roboto&display=swap');
+    font-family: 'Roboto', sans-serif;
+`;
 
 const PhotoCard = props => {
     return (
-        <div>
-            <h2>{props.title}</h2>
+        <PhotoContainer>
+            <Title>{props.title}</Title>
             <img src={props.url}></img>
-            <p>Date: {props.date}</p>
-            <p>Explanation: {props.explanation}</p>
-        </div>
+            <CardP>Date: {props.date}</CardP>
+            <CardP>Explanation: {props.explanation}</CardP>
+        </PhotoContainer>
     );
 };
 
